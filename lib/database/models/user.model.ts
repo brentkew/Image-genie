e7e78c4a7238d4bc45f2723ff631b7c2f126f2b6
Khalil Ahmad <brentkew@gmail.com>
2024-04-25 15:37:25 +0500

    Added back username changes for clerk

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,36 +1,32 @@
-import { Schema, model, models } from "mongoose";
+// const mongoose = require('mongoose');
+import { Document, Schema, model, models } from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+export interface IUser extends Document {
+  clerkId: number;
+  email: string;
+  username: string;
+  photo: string;
+  firstName?: string;
+  lastName?: string;
+  planId?: number;
+  creditBalance?: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 const UserSchema = new Schema({
-  clerkId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  photo: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  planId: {
-    type: Number,
-    default: 1,
-  },
-  creditBalance: {
-    type: Number,
-    default: 10,
-  },
+  clerkId: { type: Number, required: true, unique: true },
+  email: {type: String, required: true, unique: true},
+  username: {type: String, required: true, unique: true},
+  photo: {type: String},
+  firstName: {type: String},
+  lastName: {type: String},
+  planId: {type: Number, default: 1},
+  creditBalance: {type: Number, default: 100},
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
-const User = models?.User || model("User", UserSchema);
-
+const User = models?.User || model('User', UserSchema);
 export default User;
